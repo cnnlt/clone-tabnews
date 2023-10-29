2646f74cb5f0948d68afcdbcf0b40b8a26baea79
cnnlt <146424670+cnnlt@users.noreply.github.com>
2023-10-29 04:04:46 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>O capitalismo falhou, falha e falhará em cada uma das sociedades
+        onde ele colocar os seus tentáculos que se baseiam na expropriação e
+        na exploração do homem pelo homem, é isso que combatemos!</h1>
+}
+
+export default Home;
