312be304feaec803f06c6db7410eba683d03c567
cnnlt <146424670+cnnlt@users.noreply.github.com>
2023-10-29 08:26:28 +0000

    modificada a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <h1>O capitalismo falhou, falha e falhará em cada uma das sociedades
         onde ele colocar os seus tentáculos que se baseiam na expropriação e
-        na exploração do homem pelo homem, é isso que combatemos!</h1>
+        na exploração do homem pelo homem, é isso que combatemos! -Carvalho, João</h1>
+        
 }
 
 export default Home;
