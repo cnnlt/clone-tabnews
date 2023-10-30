aaf63b007305ed65776f753a00400caa2f55759f
cnnlt <146424670+cnnlt@users.noreply.github.com>
2023-10-30 07:11:58 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Iniciando os estudos.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Tem hora que dá medo, mas tem medo que é da hora! Love you cenoura! </h1>
+  return (
+    <h1>
+      Tem hora que dá medo, mas tem medo que é da hora! Love you cenoura!{" "}
+    </h1>
+  );
 }
 
 export default Home;
