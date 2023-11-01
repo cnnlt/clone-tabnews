91982d95e2382bc348623d0c300cd7a77d49d97f
cnnlt <146424670+cnnlt@users.noreply.github.com>
2023-11-01 00:49:58 +0000

    frase modificada

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Tem hora que dá medo, mas tem medo que é da hora! Love you cenoura!{" "}
-    </h1>
-  );
+  return <h1>🚧 Em construção... 🚧 </h1>;
 }
 
 export default Home;
